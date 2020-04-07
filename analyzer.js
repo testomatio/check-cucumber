bf029d7ee965dc345384cc375ce9a0a482884a6d
Koushik Mohan <koushikmohan1996@gmail.com>
2020-04-07 22:45:32 +0000

    Add Eslint (#6)
    * Add emojis
    
    * Add success and failure messages
    
    * Add success and failure messages
    
    * Add eslint

--- analyzer.js
+++ analyzer.js
@@ -1,73 +1,15 @@
-const Gherkin = require('gherkin').default
-const glob = require('glob')
+const Gherkin = require('gherkin').default;
+const glob = require('glob');
 const path = require('path');
-const chalk = require('chalk');
 
 let workDir;
-
-/**
- * 
- * @param {String} filePattern 
- * @param {String} dir 
- */
-const analyzeFeatureFiles = (filePattern, dir = '.') => {
-  workDir = dir;
-
-  console.log('\nParsing files\n');
-  pattern = path.join(dir, filePattern);
-
-  const promise = new Promise((resolve, reject) => {
-    const promiseArray = [];
-    glob(pattern, (er, files) => {
-      for (const file of files) {
-        promiseArray.push(parseFile(file));
-      }
-
-      const resultArray = Promise.all(promiseArray);
-      resultArray.then(resolve)
-    });
-  });
-
-  return promise;
-};
-
-const parseFile = file => {
-  return new Promise((resolve, reject) => {
-    try {
-      const options = {
-        includeSource: true,
-        includeGherkinDocument: true,
-        includePickles: true,
-      }
-      const stream = Gherkin.fromPaths([file], options)
-      const data = [];
-      const featureData = {};
-      stream.on('data', function (chunk) {
-        data.push(chunk);
-      });
-
-      stream.on('end', function () {
-        console.log('___________________________');
-        const fileName = file.replace(workDir + path.sep, '');
-        console.log('File : ', fileName , '\n');
-        console.log('= ', data[1].gherkinDocument.feature.name);
-        featureData['feature'] = data[1].gherkinDocument.feature.name;
-        featureData['scenario'] = getScenarioCode(data[0].source.data, data[1].gherkinDocument.feature, file);
-        console.log('\n');
-        resolve(featureData);
-      });
-    } catch (e) {
-      reject(e);
-    }
-  })
-};
 
 const getScenarioCode = (source, feature, file) => {
   const sourceArray = source.split('\n');
   const fileName = file.replace(workDir + path.sep, '');
   const scenarios = [];
-  for (let i = 0; i < feature.children.length; i++) {
-    const scenario = feature.children[i].scenario;
+  for (let i = 0; i < feature.children.length; i += 1) {
+    const { scenario } = feature.children[i];
     console.log(' - ', scenario.name);
     if (scenario) {
       const steps = [];
@@ -78,13 +20,71 @@ const getScenarioCode = (source, feature, file) => {
       for (const step of scenario.steps) {
         steps.push(step.text);
       }
-      scenarioJson['code'] = sourceArray.slice(start, end).join('\n');
-      scenarioJson['steps'] = steps;
+      scenarioJson.code = sourceArray.slice(start, end).join('\n');
+      scenarioJson.steps = steps;
       scenarios.push(scenarioJson);
     }
   }
 
   return scenarios;
 };
+
+const parseFile = file => {
+  return new Promise((resolve, reject) => {
+    try {
+      const options = {
+        includeSource: true,
+        includeGherkinDocument: true,
+        includePickles: true,
+      };
+      const stream = Gherkin.fromPaths([file], options);
+      const data = [];
+      const featureData = {};
+      stream.on('data', (chunk) => {
+        data.push(chunk);
+      });
+
+      stream.on('end', () => {
+        console.log('___________________________\n');
+        const fileName = file.replace(workDir + path.sep, '');
+        console.log(' 🗒️  File : ', fileName, '\n');
+        console.log('= ', data[1].gherkinDocument.feature.name);
+        featureData.feature = data[1].gherkinDocument.feature.name;
+        featureData.scenario = getScenarioCode(data[0].source.data, data[1].gherkinDocument.feature, file);
+        console.log('\n');
+        resolve(featureData);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+/**
+ *
+ * @param {String} filePattern
+ * @param {String} dir
+ */
+const analyzeFeatureFiles = (filePattern, dir = '.') => {
+  workDir = dir;
+
+  console.log('\n 🗄️  Parsing files\n');
+  const pattern = path.join(dir, filePattern);
+
+  const promise = new Promise((resolve) => {
+    const promiseArray = [];
+    glob(pattern, (er, files) => {
+      for (const file of files) {
+        promiseArray.push(parseFile(file));
+      }
+
+      const resultArray = Promise.all(promiseArray);
+      resultArray.then(resolve);
+    });
+  });
+
+  return promise;
+};
+
 
 module.exports = analyzeFeatureFiles;
